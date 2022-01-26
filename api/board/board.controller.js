c6e933c05e002c5288291fa3101ff526fac305a5
Tom Ofek Rytt <tomofekrytt@gmail.com>
2022-01-26 18:09:37 +0200

    added support from adding board from back

--- api/board/board.controller.js
+++ api/board/board.controller.js
@@ -3,73 +3,73 @@ const logger = require('../../services/logger.service');
 
 // GET LIST
 async function getBoards(req, res) {
-  // cosole.log('res', res);
-  try {
-    var queryParams = req.query;
-    const boards = await boardService.query(queryParams);
-    console.log(boards);
-    res.json(boards);
-  } catch (err) {
-    logger.error('Failed to get boards', err);
-    res.status(500).send({ err: 'Failed to get boards' });
-  }
+	// cosole.log('res', res);
+	try {
+		var queryParams = req.query;
+		const boards = await boardService.query(queryParams);
+		// console.log(boards);
+		res.json(boards);
+	} catch (err) {
+		logger.error('Failed to get boards', err);
+		res.status(500).send({err: 'Failed to get boards'});
+	}
 }
 
 // GET BY ID
 async function getBoardById(req, res) {
-  try {
-    // console.log(req.params.id);
-    const boardId = req.params.id;
-    const board = await boardService.getById(boardId);
-    console.log(board);
-    res.json(board);
-  } catch (err) {
-    logger.error('Failed to get board', err);
-    res.status(500).send({ err: 'Failed to get board' });
-  }
+	try {
+		// console.log(req.params.id);
+		const boardId = req.params.id;
+		const board = await boardService.getById(boardId);
+		// console.log(board);
+		res.json(board);
+	} catch (err) {
+		logger.error('Failed to get board', err);
+		res.status(500).send({err: 'Failed to get board'});
+	}
 }
 
 // POST (add board)
 async function addBoard(req, res) {
-  console.log('the added board function ');
-  try {
-    const board = req.body;
-    const addedBoard = await boardService.add(board);
-    res.json(addedBoard);
-  } catch (err) {
-    logger.error('Failed to add board', err);
-    res.status(500).send({ err: 'Failed to add board' });
-  }
+	// console.log('the added board function ');
+	try {
+		const board = req.body;
+		const addedBoard = await boardService.add(board);
+		res.json(addedBoard);
+	} catch (err) {
+		logger.error('Failed to add board', err);
+		res.status(500).send({err: 'Failed to add board'});
+	}
 }
 
 // PUT (Update board)
 async function updateBoard(req, res) {
-  try {
-    const board = req.body;
-    const updatedBoard = await boardService.update(board);
-    res.json(updatedBoard);
-  } catch (err) {
-    logger.error('Failed to update board', err);
-    res.status(500).send({ err: 'Failed to update board' });
-  }
+	try {
+		const board = req.body;
+		const updatedBoard = await boardService.update(board);
+		res.json(updatedBoard);
+	} catch (err) {
+		logger.error('Failed to update board', err);
+		res.status(500).send({err: 'Failed to update board'});
+	}
 }
 
 // DELETE (Remove board)
 async function removeBoard(req, res) {
-  try {
-    const boardId = req.params.id;
-    const removedId = await boardService.remove(boardId);
-    res.send(removedId);
-  } catch (err) {
-    logger.error('Failed to remove board', err);
-    res.status(500).send({ err: 'Failed to remove board' });
-  }
+	try {
+		const boardId = req.params.id;
+		const removedId = await boardService.remove(boardId);
+		res.send(removedId);
+	} catch (err) {
+		logger.error('Failed to remove board', err);
+		res.status(500).send({err: 'Failed to remove board'});
+	}
 }
 
 module.exports = {
-  getBoards,
-  getBoardById,
-  addBoard,
-  updateBoard,
-  removeBoard,
+	getBoards,
+	getBoardById,
+	addBoard,
+	updateBoard,
+	removeBoard,
 };
--- api/board/board.service.js
+++ api/board/board.service.js
@@ -3,79 +3,77 @@ const logger = require('../../services/logger.service');
 const ObjectId = require('mongodb').ObjectId;
 
 async function query() {
-  try {
-    // const criteria = _buildCriteria(filterBy);
-    const criteria = {};
+	try {
+		// const criteria = _buildCriteria(filterBy);
+		const criteria = {};
 
-    const collection = await dbService.getCollection('board');
-    // console.log('collection', collection);
-    var boards = await collection.find(criteria).toArray();
-    console.log(boards);
+		const collection = await dbService.getCollection('board');
+		// console.log('collection', collection);
+		var boards = await collection.find(criteria).toArray();
+		// console.log(boards);
 
-    return boards;
-  } catch (err) {
-    logger.error('cannot find boards', err);
-    throw err;
-  }
+		return boards;
+	} catch (err) {
+		logger.error('cannot find boards', err);
+		throw err;
+	}
 }
 
 async function getById(boardId) {
-  try {
-    const collection = await dbService.getCollection('board');
-    const board = collection.findOne({ _id: ObjectId(boardId) });
-    console.log(board);
-    return board;
-  } catch (err) {
-    logger.error(`while finding board ${boardId}`, err);
-    throw err;
-  }
+	try {
+		const collection = await dbService.getCollection('board');
+		const board = collection.findOne({_id: ObjectId(boardId)});
+		// console.log(board);
+		return board;
+	} catch (err) {
+		logger.error(`while finding board ${boardId}`, err);
+		throw err;
+	}
 }
 
 async function remove(boardId) {
-  try {
-    const collection = await dbService.getCollection('board');
-    await collection.deleteOne({ _id: ObjectId(boardId) });
-    return boardId;
-  } catch (err) {
-    logger.error(`cannot remove board ${boardId}`, err);
-    throw err;
-  }
+	try {
+		const collection = await dbService.getCollection('board');
+		await collection.deleteOne({_id: ObjectId(boardId)});
+		return boardId;
+	} catch (err) {
+		logger.error(`cannot remove board ${boardId}`, err);
+		throw err;
+	}
 }
 
 async function add(board) {
-  board.createdAt = Date.now();
-  try {
-    const collection = await dbService.getCollection('board');
-    const addedBoard = await collection.insertOne(board);
-    return addedBoard;
-  } catch (err) {
-    logger.error('cannot insert board', err);
-    throw err;
-  }
+	board.createdAt = Date.now();
+	try {
+		const collection = await dbService.getCollection('board');
+		await collection.insertOne(board);
+		console.log(board);
+		return board;
+	} catch (err) {
+		logger.error('cannot insert board', err);
+		throw err;
+	}
 }
 async function update(board) {
-  try {
-    var id = ObjectId(board._id);
-    delete board._id;
-    const collection = await dbService.getCollection('board');
-    const updatedBoard = await collection.updateOne(
-      { _id: id },
-      { $set: { ...board } }
-    );
-    console.log('updatedBoard', updatedBoard);
-    board._id = id;
-    console.log('this is the board without id ', board);
-    return board;
-  } catch (err) {
-    logger.error(`cannot update board ${boardId}`, err);
-    throw err;
-  }
+	try {
+		var id = ObjectId(board._id);
+		delete board._id;
+		const collection = await dbService.getCollection('board');
+		const updatedBoard = await collection.updateOne({_id: id}, {$set: {...board}});
+		// console.log('updatedBoard', updatedBoard);
+		board._id = id;
+		// console.log('this is the board without id ', board);
+		return board;
+	} catch (err) {
+		logger.error(`cannot update board ${boardId}`, err);
+		throw err;
+	}
 }
 
 module.exports = {
-  remove,
-  query,
-  getById,
-  add,
-  update,
+	remove,
+	query,
+	getById,
+	add,
+	update,
 };
